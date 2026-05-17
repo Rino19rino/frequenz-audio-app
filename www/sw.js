b2ee7b3df763f1a147e22b16f3386240f7f17fc2
Kathrin Frinke <aimeefrinke@MacBook-Air-von-Aimee.local>
2026-05-17 19:14:54 +0200

    feat: PWA installierbar (iOS & Android) + mobile.html abgespeckte Version

---
+++ www/sw.js
@@ -0,0 +1,5 @@
+const CACHE='rife-v1';
+const ASSETS=['/frequenz-audio-app/','/frequenz-audio-app/index.html','/frequenz-audio-app/mobile.html','/frequenz-audio-app/logo.svg','/frequenz-audio-app/manifest.json','/frequenz-audio-app/icon-192.png','/frequenz-audio-app/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
